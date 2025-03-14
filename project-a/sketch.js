94666c7aa0ca0f47eb71a9b9400b1449373efd34
Leon Eckert <leoneckert@mailbox.org>
2025-03-14 15:05:49 +0800

    changed background

--- project-a/sketch.js
+++ project-a/sketch.js
@@ -1,32 +1,43 @@
+// LEON
+
+// rebecca
+// kiran
+// joey
+// hermione
+// yining
+// emma
+// alibi
+// jane
+// sagrario
+// isaac
+
+
 function setup() {
-    // line below in web editor:
-    // createCanvas(800, 500);
-   
-    // lines below in vs code:
-    let canvas = createCanvas(800, 500);
-    canvas.id("p5-canvas");
-    canvas.parent("p5-canvas-container");
-    
-  }
+  // on p5 web editor
+  // createCanvas(800, 500);
   
-  function draw() {
-    background(100, 50, 230);
-    
-    // i made\change
-    
-    
-    push();
-    translate(200, 200-sin(radians(frameCount+0)*10)*5);
-    for(let i = 0; i < 40; i++){
-      let x = i;
-      let y = sin(radians(frameCount+i)*10)*5
-      fill('pink')
-      circle(x, y, 3);
-    }
-    pop();
-    fill(0)
+  // in VS Code:
+  let canvas = createCanvas(800, 500);
+  canvas.id("p5-canvas");
+  canvas.parent("p5-canvas-container");
+}
+
+function draw() {
+  background(220, 30, 50);
+  
+  
+  push();
+  translate(200, 200-sin(radians(frameCount+0)*10)*5);
+  for(let i = 0; i < 40; i++){
+    let x = i;
+    let y = sin(radians(frameCount+i)*10)*5
     fill('pink')
-    circle(200-20-20, 200-20, 20)
-    ellipse(200-20, 200, 40, 30)
-    
+    circle(x, y, 3);
   }
+  pop();
+  fill(0)
+  fill('pink')
+  circle(200-20-20, 200-20, 20)
+  ellipse(200-20, 200, 40, 30)
+  
+}
